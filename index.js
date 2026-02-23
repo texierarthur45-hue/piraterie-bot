128b018b0ecd0762b6ef80a2516122dad7ec2f83
texierarthur45-hue <texierarthur45@gmail.com>
2026-02-23 15:53:45 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log('PIRATERIE est en ligne !');
+});
+
+client.login(process.env.TOKEN);
